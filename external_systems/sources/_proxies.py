48836347459047ced3ca088ef08e7060fb7a00f0
Usman Zaheer <uzaheer@palantir.com>
2025-05-08 15:18:58 -0600

    comment

--- external_systems/sources/_proxies.py
+++ external_systems/sources/_proxies.py
@@ -24,7 +24,7 @@ class WrappedSession(Session):
     """
     A wrapper for requests.Session to override 'verify' property, ignoring REQUESTS_CA_BUNDLE environment variable.
 
-    This is a workaround for https://github.com/kennethreitz/requests/issues/3829 (will be fixed in requests 3.0.0)
+    This is a workaround for https://github.com/psf/requests/issues/3829 (will be fixed in requests 3.0.0)
     """
 
     def merge_environment_settings(self, url, proxies, stream, verify, *args, **kwargs):  # type: ignore[no-untyped-def]
