c7296930f3950f9dba4ae8fc377ee68c25a5f9c8
Usman Zaheer <uzaheer@palantir.com>
2025-05-08 14:52:23 -0600

    fix

--- external_systems/sources/_proxies.py
+++ external_systems/sources/_proxies.py
@@ -20,6 +20,19 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
 
+class WrappedSession(Session):
+    """
+    A wrapper for requests.Session to override 'verify' property, ignoring REQUESTS_CA_BUNDLE environment variable.
+
+    This is a workaround for https://github.com/kennethreitz/requests/issues/3829 (will be fixed in requests 3.0.0)
+    """
+    def merge_environment_settings(self, url, proxies, stream, verify, *args, **kwargs):
+        if isinstance(self.verify, str):
+            verify = self.verify
+
+        return super(WrappedSession, self).merge_environment_settings(url, proxies, stream, verify, *args, **kwargs)
+
+
 class RetryingTimeoutHttpAdapter(HTTPAdapter):
     def __init__(
         self,
@@ -83,7 +96,7 @@ def create_proxy_session(proxy_url: str, proxy_token: str) -> Session:
     """
 
     adapter = ProxyAdapter(proxy_auth={proxy_url: f"Bearer {proxy_token}"})
-    session = Session()
+    session = WrappedSession()
     session.mount("http://", adapter)
     session.mount("https://", adapter)
     session.proxies = {"all": proxy_url}
@@ -110,7 +123,7 @@ def create_session(
         timeout (Optional[Union[float, tuple[float, float], tuple[float, None]]]): Timeout settings for the session.
     """
 
-    session = Session()
+    session = WrappedSession()
 
     if cert:
         session.cert = cert
