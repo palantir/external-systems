f8f8ea0229b48ab047e1642a749454ff3db27c42
Usman Zaheer <uzaheer@palantir.com>
2025-05-08 14:59:09 -0600

    checks

--- external_systems/sources/_proxies.py
+++ external_systems/sources/_proxies.py
@@ -27,7 +27,7 @@ class WrappedSession(Session):
     This is a workaround for https://github.com/kennethreitz/requests/issues/3829 (will be fixed in requests 3.0.0)
     """
 
-    def merge_environment_settings(self, url, proxies, stream, verify, *args, **kwargs):
+    def merge_environment_settings(self, url, proxies, stream, verify, *args, **kwargs):  # type: ignore[no-untyped-def]
         if isinstance(self.verify, str):
             verify = self.verify
 
